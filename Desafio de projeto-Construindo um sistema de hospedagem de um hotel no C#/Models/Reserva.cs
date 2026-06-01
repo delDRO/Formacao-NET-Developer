b2210648e5b65fb9c6f84edd1ab166284454915d
delDRO <danielrodri189@gmail.com>
2026-06-01 16:54:59 -0400

    Criação de Repositório

---
+++ Desafio de projeto-Construindo um sistema de hospedagem de um hotel no C#/Models/Reserva.cs
@@ -0,0 +1,55 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva()
+        {
+            Hospedes = new List<Pessoa>();
+        }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+            Hospedes = new List<Pessoa>();
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= Hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes?.Count ?? 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.10m);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# 🎓 Formação .NET Developer — DIO
+
+Repositório destinado a concentrar todos os códigos, desafios de projeto, anotações e atividades práticas desenvolvidos ao longo da **Formação .NET Developer** da [DIO (Digital Innovation One)](https://www.dio.me/).
+
+---
+
+## 📋 Detalhes da Formação
+
+*   **Nível:** Intermediário
+*   **Carga Horária:** 93 horas
