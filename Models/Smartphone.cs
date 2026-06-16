f7dc9dcbe234ffa8358ce1c8e63b19918a32732a
delDRO <danielrodri189@gmail.com>
2026-06-15 22:00:30 -0400

    Desafio-Poo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,25 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria = 64)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+        }
+
+
+        private Smartphone(int memoria)
+        {
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345");
+Iphone iphone = new Iphone("987654321", "iPhone 13", "2987654321098765");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
